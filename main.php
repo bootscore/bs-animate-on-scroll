1a4abe0cb48c33e2aa4383ccccfd2f34a540d870
craftwerk <pst@crftwrk.de>
2021-02-16 10:02:23 +0100

    5.0.0.1

--- main.php
+++ main.php
@@ -2,7 +2,7 @@
 /*Plugin Name: bS5 Animate On Scroll
 Plugin URI: https://bootscore.me/plugins/bs-animate-on-scroll/
 Description: This plugin registers and initializes AOS library https://michalsnik.github.io/aos/ in WordPress. You can then use the animations by writing them into your HTML code like &lt;div data-aos="zoom-in-down"&gt;&lt;/div&gt;.
-Version: 5.0.0
+Version: 5.0.0.1
 Author: Bastian Kreiter
 Author URI: https://crftwrk.de
 License: GPLv2
@@ -11,11 +11,9 @@ License: GPLv2
 
 // Register Styles and Scripts
 function bs_aos_scripts() {
+    
+    wp_enqueue_script( 'aos-js', plugins_url( '/js/aos.js' , __FILE__ ), array( 'jquery' ), '1.0', true );
 
-    wp_enqueue_script( 'aos-js', plugins_url( '/js/aos.js', __FILE__ ));
-    
-    wp_enqueue_script( 'initialize', plugins_url( '/js/initialize.js', __FILE__ ));
-    
     wp_register_style( 'aos-css', plugins_url('css/aos.css', __FILE__) );
         wp_enqueue_style( 'aos-css' );
         
--- readme.txt
+++ readme.txt
@@ -3,13 +3,18 @@
 Contributors: craftwerk
 
 Requires at least: 4.5
-Tested up to: 5.3.2
+Tested up to: 5.6
 Requires PHP: 5.6
-Stable tag: 5.0.0
+Stable tag: 5.0.0.1
 License: GNU General Public License v2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
-Animate On Scroll by Michał Sajnóg https://michalsnik.github.io/aos/ Plugin for WordPress, Copyright 2020 Bastian Kreiter.
+Animate On Scroll by Michał Sajnóg https://michalsnik.github.io/aos/ Plugin for WordPress, Copyright 2021 Bastian Kreiter.
+
+
+== License & Credits ==
+
+Animate On Scroll, Michał Sajnóg, MIT License https://github.com/michalsnik/aos/blob/next/LICENSE
 
 
 == Installation ==
@@ -27,6 +32,11 @@ Use it in your HTML like this:
 
 
 == Changelog ==
+
+    = 5.0.0.1 - February 16 2021 =
+    
+        * [SEO] Load js in footer
+        * [SEO] Merged aos.js and aos-init.js to one file to reduce HTTP requests
 
     = 5.0.0 - December 30 2020 =
     
